e9832bf5f73481acd779129615aab49c0e0f679b
gscdae <972463280@qq.com>
2018-06-08 10:12:44 +0800

    save

--- src/apis/channelApi.js
+++ src/apis/channelApi.js
@@ -1,10 +1,10 @@
 import http from '../config/httpConfig'
 
-const searchChannelList = () => http.get('/channel/channellist', {mock: true})
+const searchChannelList = () => http.get('/channel/channellist', {mock: false})
 
-const searchMerchantsList = () => http.get('/channel/merchantsList', {mock: true})
+const searchMerchantsList = () => http.get('/channel/merchantsList', {mock: false})
 
-const searchContractList = () => http.get('/channel/constractList', {mock: true})
+const searchContractList = () => http.get('/channel/constractList', {mock: false})
 
 export default {
     searchChannelList,
