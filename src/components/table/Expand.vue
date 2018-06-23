64cf754455f1361a3704f771391068c46e1e40c6
gscdae <972463280@qq.com>
2018-06-23 14:52:44 +0800

    部分bug修改

--- src/components/table/Expand.vue
+++ src/components/table/Expand.vue
@@ -1,33 +1,38 @@
-<style scoped>
-    .expand-row{
-        margin-bottom: 16px;
-    }
-</style>
 <template>
     <div>
         <Row class="expand-row">
             <Col span="8">
-                <span class="expand-key">渠道AK: </span>
+                <label class="expand-key">渠道AK
+                    <Tooltip content="渠道后台系统调用部平台接口的接入用户名" placement="top-start">
+                        <Icon class="info" type="information-circled"></Icon>
+                    </Tooltip>: </label>
                 <span class="expand-value">{{ row.channelAccessKey }}</span>
             </Col>
             <Col span="8">
-                <span class="expand-key">渠道SK: </span>
+                <label class="expand-key">渠道SK
+                    <Tooltip content="渠道后台系统调用部平台接口的接入密钥" placement="top-start">
+                        <Icon class="info" type="information-circled"></Icon>
+                    </Tooltip>: </label>
                 <span class="expand-value">{{ row.channelSecretKey }}</span>
-            </Col>
-            <Col span="8">
-                <span class="expand-key">签名算法: </span>
-                <span class="expand-value">{{ row.signAlgorithm }}</span>
             </Col>
         </Row>
         <Row>
             <Col span="8">
-                <span class="expand-key">签名盐值: </span>
-                <span class="expand-value">{{ row.algorithmValue }}</span>
+                <label class="expand-key">签名算法
+                    <Tooltip content="渠道APP与部平台H5页面交互时的算法" placement="top-start">
+                        <Icon class="info" type="information-circled"></Icon>
+                    </Tooltip>: </label>
+                <span class="expand-value">{{ row.signAlgorithm }}</span>
             </Col>
             <Col span="8">
-                <span class="expand-key">加密密钥: </span>
+                <label class="expand-key">数据密钥
+                    <Tooltip content="渠道APP与部平台H5页面交互时的密钥" placement="top-start">
+                        <Icon class="info" type="information-circled"></Icon>
+                    </Tooltip>: </label>
                 <span class="expand-value">{{ row.encryptKey }}</span>
-                <Icon type="refresh" v-on:click="refresh(row)"></Icon>
+                <Tooltip content="切换数据密钥" placement="top-start">
+                    <Icon class="expend-icon" :class="{'run': isRun}" :type="refreshIcon" @click.native="refresh(row)"></Icon>
+                </Tooltip>
             </Col>
         </Row>
     </div>
@@ -39,15 +44,63 @@ export default {
     props: {
         row: Object
     },
+    data () {
+        return {
+            isRun: false,
+            refreshIcon: 'refresh'
+        }
+    },
     methods: {
         refresh (v) {
+            if (this.isRun) return
+            this.isRun = true
+            this.refreshIcon = 'load-c'
             let {channelId} = {...v}
             channelApi.updateEncryptKey(channelId).then(
                 ({data: {result, resultCode, msg}}) => {
+                    this.isRun = false
+                    this.refreshIcon = 'refresh'
                     v.encryptKey = result
                 }
-            )
+            ).catch(() => {
+                this.isRun = false
+                this.refreshIcon = 'refresh'
+            })
         }
     }
 }
 </script>
+<style lang="less" scoped>
+.expand-row{
+    margin-bottom: 16px;
+}
+.info {
+    color: rgb(212, 212, 212);
+    padding-right: 5px;
+}
+.expand-key {
+    font-weight: 700;
+    width: 80px;
+    text-align: right;
+    display: inline-block;
+    margin-right: 10px;
+}
+.expend-icon {
+    color: #2d8cf0;
+    cursor: pointer;
+    font-size: 16px;
+    margin-left: 10px;
+}
+@-webkit-keyframes rotation{
+    from {-webkit-transform: rotate(0deg);}
+    to {-webkit-transform: rotate(360deg);}
+}
+
+.run {
+    -webkit-transform: rotate(360deg);
+    animation: rotation 1s linear infinite;
+    -moz-animation: rotation 1s linear infinite;
+    -webkit-animation: rotation 1s linear infinite;
+    -o-animation: rotation 1s linear infinite;
+}
+</style>
